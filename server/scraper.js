c596faf20c7a2109d1b8abaca329b41a373446cd
Falk <petruk0908@gmail.com>
2020-05-21 12:31:07 +0300

    finish scraper

--- server/scraper.js
+++ server/scraper.js
@@ -1,26 +1,70 @@
 const { workerData, parentPort } = require("worker_threads");
 const puppeteer = require("puppeteer");
-
+const linksGatherer = require("./utils/linksGatherer");
+const CheckedNode = require('./utils/CheckedNode');
 console.log("i scraper");
 
-let scrape = async () => {
+
+
+let scrape = async (url) => {
+  let domain = new URL(url).hostname;
+  const queue = [];
+  const checkedNodes = [];
+  const edges = [];
+  let checkedCount = 0;
+  queue.push(url);
+
   const browser = await puppeteer.launch();
+
   const page = await browser.newPage();
 
-  await page.goto(workerData);
 
-  const result = await page.evaluate(() => {
-    let links = Array.prototype.map.call(
-      document.querySelectorAll("a[href]"),
-      (link) => link.href
-    );
-    return links;
-  });
+
+
+  let generateEdges = (from, newLinks) => {
+    newLinks.forEach((link) => {
+      try {
+        let linkObj = new URL(link);
+
+        edges.push({ from: from, to: link });
+        if (!checkedNodes.find((node) => node.id == link)) {
+
+          if (linkObj.hostname !== domain) {
+            checkedNodes.push(new CheckedNode(link, false));
+          } else {
+            if (!queue.find((url) => url == link)) queue.push(link);
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+
+    });
+  };
+
+  while (checkedCount < 50) {
+
+    let checkingNode = queue.shift();
+
+    if (checkingNode) {
+
+      checkedNodes.push(new CheckedNode(checkingNode));
+
+      await page.goto(checkingNode);
+      const newLinks = await page.evaluate(linksGatherer);
+
+      checkedCount++;
+      console.log(checkedCount);
+
+      generateEdges(checkingNode, newLinks);
+    }
+  }
 
   browser.close();
-  return result;
+  checkedNodes[0].color = 'red';
+  return { nodes: checkedNodes, edges: edges };
 };
 
-scrape().then((value) => {
-  parentPort.postMessage(value);
+scrape(workerData).then((graph) => {
+  parentPort.postMessage(graph);
 });
